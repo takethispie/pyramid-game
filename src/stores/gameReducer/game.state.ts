05b960a9abc57b360211f7c7f7899f96e1cf5809
Sebastien Klasa <klasa.sebastien@gmail.com>
2020-03-23 15:38:35 +0000

    [game] add getStep

--- src/stores/gameReducer/game.state.ts
+++ src/stores/gameReducer/game.state.ts
@@ -12,3 +12,42 @@ export interface GameState {
     Accusations: { [id: string]: string },
     Sips: { [id: string]: number }
 }
+
+function allSipsAreZero(game: GameState): boolean {
+    return Object.keys(game.Sips).every(player => game.Sips[player] == 0)
+}
+
+function targetsIsEmpty(game: GameState): boolean {
+    return Object.keys(game.Targets).length == 0
+}
+
+function targetsIsFull(game: GameState): boolean {
+    return [...game.Players].every(player => game.Targets[player] != undefined)
+}
+
+function targetsIsPartial(game: GameState): boolean {
+    return !targetsIsEmpty(game) && !targetsIsFull(game)
+}
+
+function accusationsIsEmpty(game: GameState): boolean {
+    return Object.keys(game.Accusations).length == 0
+}
+
+export function getStep(game: GameState): GameStep | undefined {
+    const targetsEmpty = targetsIsEmpty(game)
+    const targetsPartial = targetsIsPartial(game)
+    const targetsFull = targetsIsFull(game)
+    const accusationsEmpty = accusationsIsEmpty(game)
+    const allSipsZero = allSipsAreZero(game)
+    if (!targetsFull && accusationsEmpty && allSipsZero) {
+        return GameStep.ChooseTarget
+    } else if (targetsPartial && (!accusationsEmpty || !allSipsZero) || targetsFull && accusationsEmpty && allSipsZero) {
+        return GameStep.Accuse
+    } else if (targetsEmpty && !accusationsEmpty) {
+        return GameStep.Deny
+    } else if (targetsEmpty && accusationsEmpty && !allSipsZero) {
+        return GameStep.Drink
+    } else {
+        return undefined
+    }
+}
