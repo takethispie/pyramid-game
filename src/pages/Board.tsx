c2df92b660defe7744bdb18fb3b3b33025b70e69
Sebastien Klasa <klasa.sebastien@gmail.com>
2020-04-02 04:18:56 +0200

    add controls to board page

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -25,23 +25,40 @@ import store from "stores";
 import Hand from "components/Hand";
 import { ThunkLoadHand } from "stores/handReducer/hand.thunk";
 import { connectToRoom } from "stores/syncMiddleware/sync.middleware";
-import { ThunkLeaveGame } from "stores/gameReducer/game.thunk";
+import { ThunkLeaveGame, ThunkChooseTarget, ThunkAcceptToDrink, ThunkAccuse, ThunkAdmitToLying, ThunkProveNotToLie, ThunkDrink } from "stores/gameReducer/game.thunk";
 import { useBeforeunload } from "react-beforeunload";
+import { getStep, GameStep } from "stores/gameReducer/game.state";
+import ChooseTargetComponent from "components/ChooseTarget";
+import AccuseComponent from "components/Accuse"
+import WaitingOtherPlayersComponent from "components/WaitingOtherPlayers"
+import DenyComponent from "components/Deny"
+import DrinkComponent from "components/Drink"
 
 const mapState = (state: RootState) => ({
-  pyramid: state.boardReducer.Pyramid,
-  boardError: state.boardReducer.ErrorMessage,
-  hand: state.handReducer.Hand,
-  nickname: state.matchReducer.NickName,
-  matchId: state.matchReducer.MatchId,
-  cardStack: state.boardReducer.CardStack
+  pyramid: state.boardReducer.Pyramid
+  , boardError: state.boardReducer.ErrorMessage
+  , hand: state.handReducer.Hand
+  , nickname: state.matchReducer.NickName
+  , matchId: state.matchReducer.MatchId
+  , cardStack: state.boardReducer.CardStack
+  , game: state.gameReducer
+  , players: state.gameReducer.Players
+  , targets: state.gameReducer.Targets
+  , accusations: state.gameReducer.Accusations
+  , sips: state.gameReducer.Sips
 });
 
 const mapDispatch = {
-  revealCard: ThunkRevealNextCard,
-  loadHand: ThunkLoadHand,
-  leaveGame: ThunkLeaveGame,
-  generatePyramid: ThunkGeneratePyramid
+  revealCard: ThunkRevealNextCard
+  , loadHand: ThunkLoadHand
+  , leaveGame: ThunkLeaveGame
+  , generatePyramid: ThunkGeneratePyramid
+  , chooseTarget: ThunkChooseTarget
+  , acceptToDrink: ThunkAcceptToDrink
+  , accuse: ThunkAccuse
+  , admitToLying: ThunkAdmitToLying
+  , proveNotToLie: ThunkProveNotToLie
+  , drink: ThunkDrink
 };
 
 const connector = connect(mapState, mapDispatch);
@@ -51,7 +68,29 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux & {};
 
-const Board: React.FC<Props> = ({ revealCard, pyramid, boardError, hand, loadHand, nickname, matchId, cardStack, leaveGame, generatePyramid }) => {
+const Board: React.FC<Props> = ({
+  revealCard
+  , pyramid
+  , boardError
+  , hand
+  , loadHand
+  , nickname
+  , matchId
+  , cardStack
+  , leaveGame
+  , generatePyramid
+  , game
+  , players
+  , targets
+  , chooseTarget
+  , acceptToDrink
+  , accuse
+  , accusations
+  , admitToLying
+  , proveNotToLie
+  , sips
+  , drink
+}) => {
 
   if (hand.length === 0) loadHand(nickname, matchId, cardStack);
 
@@ -74,6 +113,44 @@ const Board: React.FC<Props> = ({ revealCard, pyramid, boardError, hand, loadHan
   }, [connectedRoom, pyramid.length, generatePyramid])
 
   useBeforeunload(() => leaveGame());
+
+  function getControls(step: GameStep): JSX.Element {
+    switch (step) {
+      case GameStep.ChooseTarget:
+        const otherPlayers = [...players].filter(player => player != nickname)
+        if (otherPlayers.length > 0) {
+          return <ChooseTargetComponent players={otherPlayers} currentTarget={targets[nickname]} chooseTarget={player => chooseTarget(nickname, player)} />
+        } else {
+          return <WaitingOtherPlayersComponent />
+        }
+      case GameStep.Accuse:
+        let targetedByList = Object.keys(targets).filter(key => targets[key] == nickname)
+        if (targetedByList.length > 0) {
+          const playerWhoTargets = targetedByList[0]
+          let selectedButton: 'None' | 'Accuse' | 'AcceptToDrink' = 'None'
+          if (accusations[playerWhoTargets] == nickname) {
+            selectedButton = 'Accuse'
+          } else if (targets[playerWhoTargets] == undefined) {
+            selectedButton = 'AcceptToDrink'
+          }
+          return <AccuseComponent playerWhoTargets={playerWhoTargets} acceptToDrink={() => acceptToDrink(playerWhoTargets, nickname)} accuseOfLying={() => accuse(playerWhoTargets, nickname)} />
+        } else {
+          return <WaitingOtherPlayersComponent />
+        }
+      case GameStep.Deny:
+        if (accusations[nickname] != undefined) {
+          return <DenyComponent playerWhoAccuses={accusations[nickname]} proveNotToLie={() => proveNotToLie(nickname, accusations[nickname])} admitToLying={() => admitToLying(nickname, accusations[nickname])} />
+        } else {
+          return <WaitingOtherPlayersComponent />
+        }
+      case GameStep.Drink:
+        if (sips[nickname] > 0) {
+          return <DrinkComponent numberOfSips={sips[nickname]} drink={() => drink(nickname)} />
+        } else {
+          return <WaitingOtherPlayersComponent />
+        }
+    }
+  }
 
   return (
     <IonPage>
@@ -105,6 +182,20 @@ const Board: React.FC<Props> = ({ revealCard, pyramid, boardError, hand, loadHan
                 </Layer>
               </Provider>
             </Stage>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonButton onClick={() => revealCard()}>test</IonButton>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                {getControls(getStep(game)!)}
+              </IonItem>
+            </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
